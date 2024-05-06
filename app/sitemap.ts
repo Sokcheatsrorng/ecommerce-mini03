921b5557997c4b9dc17abee7e2f2d90a73180537
Sokcheatsrorng <sokcheat.srorng@gmail.com>
2024-05-06 23:22:27 +0700

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,31 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://nextjs-typescript-eta.vercel.app',
+      url: 'https://ecommerce-mini03-8ecj1pc31-sokcheats-projects.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://nextjs-typescript-eta.vercel.app/about',
+      url: 'https://ecommerce-mini03-8ecj1pc31-sokcheats-projects.vercel.app/about-us',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://nextjs-typescript-eta.vercel.app/service',
+      url: 'https://ecommerce-mini03-8ecj1pc31-sokcheats-projects.vercel.app/cart',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://ecommerce-mini03-8ecj1pc31-sokcheats-projects.vercel.app/policy',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://ecommerce-mini03-8ecj1pc31-sokcheats-projects.vercel.app/my-shop',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
